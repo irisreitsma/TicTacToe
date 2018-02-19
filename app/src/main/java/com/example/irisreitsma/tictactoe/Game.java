738198cdfeb25fc06717ef562faf5625c064f302
Iris Reitsma <irisreitsma@online.nl>
2018-02-19 14:36:00 +0100

    final version

--- app/src/main/java/com/example/irisreitsma/tictactoe/Game.java
+++ app/src/main/java/com/example/irisreitsma/tictactoe/Game.java
@@ -1,17 +1,21 @@
 package com.example.irisreitsma.tictactoe;
 
+import android.widget.Toast;
+
+import java.io.Serializable;
+
 /**
  * Created by Iris Reitsma on 13-2-2018.
  */
 
-public class Game {
+public class Game implements Serializable {
 
     // properties of the class
     final private int BOARD_SIZE = 3;
     private Tile[][] board;
     private Boolean playerOneTurn;  // true if player 1's turn, false if player 2's turn
     private int movesPlayed;
-    private Boolean gameOver;
+    private boolean gameOver;
 
     // constructor of the class
     public Game() {
@@ -25,21 +29,90 @@ public class Game {
     }
 
     // methods of the class
+
+    // draw board
     public Tile draw(int row, int column) {
-        if (board[row][column] == Tile.BLANK) {
-            if (playerOneTurn){
+        if (board[row][column] == Tile.BLANK && !gameOver) {
+            if (playerOneTurn) {
                 board[row][column] = Tile.CROSS;
                 playerOneTurn = false;
+                movesPlayed++;
                 return Tile.CROSS;
-            }
-            else {
+            } else {
                 board[row][column] = Tile.CIRCLE;
                 playerOneTurn = true;
+                movesPlayed++;
                 return Tile.CIRCLE;
             }
-        }
-        else {
+        } else {
             return Tile.INVALID;
+        }
+    }
+    // draw saved board
+    public Tile saveddraw(int row, int column) {
+        if (board[row][column] == Tile.CROSS) {
+            return Tile.CROSS;
+        } if (board[row][column] == Tile.CIRCLE) {
+            return Tile.CIRCLE;
+        } else {
+            return Tile.BLANK;
+        }
+    }
+
+    // check state of game
+    public GameState state(int row, int column) {
+        // check for win
+        for (int i=0; i<BOARD_SIZE; i++) {
+            // check rows
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                if (board[i][0] == Tile.CROSS) {
+                    gameOver = true;
+                    return GameState.PLAYER_ONE;
+                } if (board[i][0] == Tile.CIRCLE) {
+                    gameOver = true;
+                    return GameState.PLAYER_TWO;
+                }
+            }
+            // check columns
+            if (board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+                if (board[0][i] == Tile.CROSS) {
+                    gameOver = true;
+                    return GameState.PLAYER_ONE;
+                } if (board[0][i] == Tile.CIRCLE) {
+                    gameOver = true;
+                    return GameState.PLAYER_TWO;
+                }
+            }
+        }
+        // check diagonals
+        if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+            if (board[0][0] == Tile.CROSS) {
+                gameOver = true;
+                return GameState.PLAYER_ONE;
+            } if (board[0][0] == Tile.CIRCLE) {
+                gameOver = true;
+                return GameState.PLAYER_TWO;
+            }
+        }
+        if (board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+            if (board[0][2] == Tile.CROSS) {
+                gameOver = true;
+                return GameState.PLAYER_ONE;
+            } if (board[0][2] == Tile.CIRCLE) {
+                gameOver = true;
+                return GameState.PLAYER_TWO;
+            }
+        }
+
+        // check for draw
+        if (movesPlayed == 9) {
+            gameOver = true;
+            return GameState.DRAW;
+        }
+
+        // game is in progress
+        else {
+            return GameState.IN_PROGRESS;
         }
     }
 }
--- app/src/main/java/com/example/irisreitsma/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/irisreitsma/tictactoe/MainActivity.java
@@ -6,44 +6,67 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import static com.example.irisreitsma.tictactoe.Tile.CROSS;
-
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     // lists for comparison
     int buttonID[] = {R.id.button11, R.id.button12, R.id.button13, R.id.button21, R.id.button22, R.id.button23, R.id.button31, R.id.button32, R.id.button33};
-    int rows[]     = {1, 1, 1, 2, 2, 2, 3, 3, 3};
-    int columns[]  = {1, 2, 3, 1, 2, 3, 1, 2, 3};
+    int rows[]     = {0, 0, 0, 1, 1, 1, 2, 2, 2};
+    int columns[]  = {0, 1, 2, 0, 1, 2, 0, 1, 2};
 
     // properties of the class
     Game game;
     int row;
     int column;
+    int length = buttonID.length;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // reload same state
+        if (savedInstanceState != null) {
+            game = (Game) savedInstanceState.getSerializable("game");
+            for (int i = 0; i < length; i++) {
+                Tile tile = game.saveddraw(i/3, i%3);
+                Button button = findViewById(buttonID[i]);
+                switch (tile) {
+                    case CROSS:
+                        button.setText("X");
+                        break;
+                    case CIRCLE:
+                        button.setText("O");
+                        break;
+                }
+            }
+        }
+        else {
+            game = new Game();
+        }
+    }
+
+    // preserve state of game
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("game", game);
+    }
 
     // methods of the class
-    public void onCreate() {
-        game = new Game();
-    }
 
     // puts a cross or circle on correct button
     public void tileClicked(View view) {
         Button button = (Button) view;
         int id = view.getId();
-        int length = buttonID.length;
 
-        for (int i : buttonID) {
+        for (int i = 0; i < length; i++) {
             if (id == buttonID[i]) {
-                int row = rows[i];
-                int column = columns[i];
+                row = rows[i];
+                column = columns[i];
             }
         }
 
+        // updates button
         Tile tile = game.draw(row, column);
         switch (tile) {
             case CROSS:
@@ -56,15 +79,28 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "invalid move", Toast.LENGTH_SHORT).show();
                 break;
         }
+
+        // print state of game
+        GameState state = game.state(row, column);
+        switch (state) {
+            case PLAYER_ONE:
+                Toast.makeText(MainActivity.this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+                break;
+            case PLAYER_TWO:
+                Toast.makeText(MainActivity.this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+                break;
+            case DRAW:
+                Toast.makeText(MainActivity.this, "It's a draw!", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     // resets game
-    public void resetClicked() {
+    public void resetClicked(View view) {
         for (int i : buttonID) {
             Button button = findViewById(i);
             button.setText("");
         }
-
         game = new Game();
     }
 }
